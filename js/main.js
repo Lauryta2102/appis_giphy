b515cf5f854a733b0d07a128fc09759595729604
laura_ibaceta <lauraibaceta@gmail.com>
2023-08-25 21:21:51 -0300

    segunda modificacion a la appikey

--- js/main.js
+++ js/main.js
@@ -1,3 +1,12 @@
-const apikey = "";
+const apikey = "AIzaSyAPGkGKGn2efLfsXbY4rVffT1Y7MT4IoSo";
+
+const termino = document.querySelector (".termino");
+const buscador = document.querySelector (".buscador");
+const resultados = document.querySelector (".resultados");
+
+const url =`https://tenor.googleapis.com/v2/search?q=${termino.value}&key=${apikey}&client_key=my_test_app&limit=9`
+
+
+
 
 
